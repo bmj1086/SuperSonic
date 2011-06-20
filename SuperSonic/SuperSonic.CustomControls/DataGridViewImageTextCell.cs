1332fd0e291e0b69f9a1b16bcb1a4de4fa22afb7
Alex <alexvance89@gmail.com>
2011-06-20 13:10:16 -0500

    init commit

--- SuperSonic/SuperSonic.CustomControls/DataGridViewImageTextCell.cs
+++ SuperSonic/SuperSonic.CustomControls/DataGridViewImageTextCell.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace SuperSonic.CustomControls
 {
-    class DataGridViewImageTextCell
+    class DataGridViewImageTextCell : DataGridViewImageCell
     {
+        protected override void Paint(System.Drawing.Graphics graphics, 
+            System.Drawing.Rectangle clipBounds, 
+            System.Drawing.Rectangle cellBounds, 
+            int rowIndex, 
+            DataGridViewElementStates elementState, object value, object formattedValue, string errorText, DataGridViewCellStyle cellStyle, DataGridViewAdvancedBorderStyle advancedBorderStyle, DataGridViewPaintParts paintParts)
+        {
+            )base.Paint(graphics, clipBounds, cellBounds, rowIndex, elementState, value, formattedValue, errorText, cellStyle, advancedBorderStyle, paintParts);
+        }
     }
 }
--- SuperSonic/SuperSonic/MainGUI.Designer.cs
+++ SuperSonic/SuperSonic/MainGUI.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 210);
+            this.Name = "MainGUI";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
